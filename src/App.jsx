8c5f045fb38df1b5f80af3eb720daa61b9a7ad8f
Gythrie <151329547+Gythrie@users.noreply.github.com>
2025-03-02 21:13:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import AboutUs from "./pages/aboutus"
 import Prize from "./pages/prize"
 import Footer from "./pages/Footer"
 import Gallery from "./pages/gallery"
+import RefreshReboot from "./pages/refreshreboot"
 
 function App() {
   return (
     <div className="flex flex-col bg-custom-black">
     <LandingPage/>
     <Countdown/>
+    <RefreshReboot/>
     <AboutUs/>
     <Prize/>
     <Gallery/>
