e111a7a3261bf23394889f9b3bbdf41371eda02e
naevdd <naveedniaz2005@gmail.com>
2025-06-13 11:09:48 +0530

    updated FAQ sizing

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -3,7 +3,7 @@ import Card from "../components/card";
 const Gallery = () => {
   return (
     <div id="gallery" className="bg-custom-black p-8 flex flex-col items-center min-h-screen md:pt-20 pt-10">
-      <h1 className="text-custom-white font-seasons_r text-left text-4xl md:text-7xl">
+      <h1 className="text-custom-white font-seasons_r text-left text-4xl md:text-6xl">
         FREQUENTLY <span className="text-custom-yellow">ASKED QUESTIONS</span>.
       </h1>
       <div className="md:flex md:flex-row flex-col my-auto items-center gap-1 group">
@@ -14,27 +14,27 @@ const Gallery = () => {
         <Card
           defaultText="Should the project be Free and Open Source?"
           flippedText="Yes. The project should be Free and Open Source."
-          extraClasses="md:-ml-96"
+          extraClasses="md:-ml-[420px]"
         />
         <Card
           defaultText="But... I’ve never participated in a hackathon"
           flippedText="That’s okay! We welcome hackers of all skill levels. We’ll have workshops and mentors to help you learn and build something awesome."
-          extraClasses="md:-ml-96"
+          extraClasses="md:-ml-[420px]"
         />
         <Card
           defaultText="How many prizes can a team acquire?"
           flippedText="A team can only win one prize."
-          extraClasses="md:-ml-96"
+          extraClasses="md:-ml-[420px]"
         />
         <Card
           defaultText="How many members can be there in a team?"
           flippedText="Each team has to consist of 2-4 members."
-          extraClasses="md:-ml-96"
+          extraClasses="md:-ml-[420px]"
         />
         <Card
           defaultText="Is there any prerequisites for this hackathon?"
           flippedText="Not at all! We encourage you to give it a shot even if you consider yourself a beginner-level programmer."
-          extraClasses="md:-ml-96"
+          extraClasses="md:-ml-[420px]"
         />
       </div>
     </div>
