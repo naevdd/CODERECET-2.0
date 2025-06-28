44723a77723b6b4ab44a71d2d947bff7a42da5a2
Gopika Sreekumar <151044666+Gopika4112@users.noreply.github.com>
2025-06-28 12:45:38 +0530

    Update landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -109,7 +109,7 @@ function LandingPage() {
                 <li className="ml-4 lg:ml-8">
                   <div
                     className="apply-button hidden sm:block"
-                    data-hackathon-slug="coderecet-2"
+                    data-hackathon-slug="code-recet-2"
                     data-button-theme="light"
                     style={{
                       height: "44px",
