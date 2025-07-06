103a16508fe6ff35897296d93d28bb749b5c6956
Gopika4112 <gsgopikasg@gmail.com>
2025-07-06 23:19:48 +0530

    made countdown responsive

--- src/pages/countdown.jsx
+++ src/pages/countdown.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-const countdown = () => {
+const Countdown = () => {
   const formatNumber = (num) => (num < 10 ? `0${num}` : num);
 
   const calculateTimeLeft = () => {
     const targetDate = new Date(
-      "July 18, " + new Date().getFullYear() + " 00:00:00"
+      "July 25, " + new Date().getFullYear() + " 17:30:00"
     );
     const now = new Date();
     const difference = targetDate - now;
@@ -16,7 +16,7 @@ const countdown = () => {
       timeLeft = {
         days: formatNumber(Math.floor(difference / (1000 * 60 * 60 * 24))),
         hours: formatNumber(Math.floor((difference / (1000 * 60 * 60)) % 24)),
-        minutes: Math.floor((difference / (1000 * 60)) % 60),
+        minutes: formatNumber(Math.floor((difference / (1000 * 60)) % 60)),
         seconds: formatNumber(Math.floor((difference / 1000) % 60)),
       };
     } else {
@@ -79,32 +79,41 @@ const countdown = () => {
           </h1>
         </div>
         <div className="bg-custom-yellow flex justify-center items-center rounded-xl mx-4 lg:mx-8 w-auto text-[#0A0A0A] h-30 sm:h-48 md:h-56 lg:h-72 mb-6 -mt-12 lg:-mt-0">
-          <div className="flex flex-row lg:space-x-12 space-x-6 -pt-8 lg:mb-4 mb-4 text-[72px] lg:text-[230px] font-bebas_neue">
-            <div className="text-center">
-              <span>{timeLeft.days}</span>
-              <div className="lg:text-2xl text-xs -mt-4 lg:-mt-20 font-satoshi_v">
-                DAYS
-              </div>
-            </div>
-            <span>:</span>
-            <div className="text-center">
-              <span>{timeLeft.hours}</span>
-              <div className="lg:text-2xl text-xs -mt-4 lg:-mt-20 font-satoshi_v">
-                HOURS
-              </div>
-            </div>
-            <span>:</span>
-            <div className="text-center">
-              <span>{timeLeft.minutes}</span>
-              <div className="lg:text-2xl text-xs -mt-4 lg:-mt-20 font-satoshi_v">
-                MINUTES
-              </div>
-            </div>
-            <span>:</span>
-            <div className="text-center">
-              <span>{timeLeft.seconds}</span>
-              <div className="lg:text-2xl text-xs -mt-4 lg:-mt-20 font-satoshi_v">
-                SECONDS
+          <div className="w-full flex justify-center overflow-x-auto min-w-0">
+            <div className="flex flex-row ... min-w-max mx-auto">
+              <div
+                className="flex flex-row
+  text-[60px] sm:text-[96px] md:text-[120px] lg:text-[140px] xl:text-[230px]
+  space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-12
+  -pt-8 lg:mb-4 mb-4 font-bebas_neue"
+              >
+                <div className="text-center">
+                  <span>{timeLeft.days}</span>
+                  <div className="text-[14px] md:text-[20px] lg:text-2xl -mt-2 sm:-mt-4 md:-mt-8 lg:-mt-12 xl:-mt-20 font-satoshi_v">
+                    DAYS
+                  </div>
+                </div>
+                <span>:</span>
+                <div className="text-center">
+                  <span>{timeLeft.hours}</span>
+                  <div className="text-[14px] md:text-[20px] lg:text-2xl -mt-2 sm:-mt-4 md:-mt-8 lg:-mt-12 xl:-mt-20 font-satoshi_v">
+                    HOURS
+                  </div>
+                </div>
+                <span>:</span>
+                <div className="text-center">
+                  <span>{timeLeft.minutes}</span>
+                  <div className="text-[14px] md:text-[20px] lg:text-2xl -mt-2 sm:-mt-4 md:-mt-8 lg:-mt-12 xl:-mt-20 font-satoshi_v">
+                    MINUTES
+                  </div>
+                </div>
+                <span>:</span>
+                <div className="text-center">
+                  <span>{timeLeft.seconds}</span>
+                  <div className="text-[14px] md:text-[20px] lg:text-2xl -mt-2 sm:-mt-4 md:-mt-8 lg:-mt-12 xl:-mt-20 font-satoshi_v">
+                    SECONDS
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -114,4 +123,4 @@ const countdown = () => {
   );
 };
 
-export default countdown;
+export default Countdown;
