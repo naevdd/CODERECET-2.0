04c244a39fd095f6e3bd4342dff838e755c82a91
Unknown-Geek <shravanpandala@gmail.com>
2025-07-07 21:43:26 +0530

    Title sponsor added

--- src/pages/sponsorship.jsx
+++ src/pages/sponsorship.jsx
@@ -4,37 +4,34 @@ import ethindia from "../assets/ethindia-light.svg";
 import cetaaUAE from "../assets/CETA UAE LOGO.png";
 import cetaaDC from "../assets/cetaa dc.png";
 import buildclub from "../assets/image.png";
+import macrosoft from "../assets/macrosoft.png";
 
 function Sponsors() {
   return (
     <div id="sponsors" className="bg-custom-black">
       <div className="relative border-custom-gray border-t border-b text-custom-white py-20 md:py-24 px-6 md:px-20 flex flex-col items-center">
         {/* Our Sponsors Section */}
-        <h2 className="text-center text-5xl md:text-7xl font-seasons_r mb-10">
+        <h2 className="text-center text-5xl md:text-7xl font-seasons_r mb-2">
           OUR <span className="text-custom-yellow">SPONSORS</span>.
         </h2>
-        <div className="flex flex-col md:flex-col w-full max-w-screen-xl space-y-4">
-          {/* <div className="border border-custom-yellow rounded-[15px] p-8 h-24 md:h-32 w-full flex items-center justify-center">
-            <img
-                src={cetaa}
-                alt="CETAA UAE LOGO"
-                className="max-h-20 md:max-h-24 object-contain"
-              />
-          </div> */}
-          <div className="border border-custom-yellow rounded-[15px] p-8 h-20 md:h-28 w-full flex items-center justify-center">
-            <a
-              href="https://devfolio.co"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={devfolio}
-                alt="DEVFOLIO LOGO"
-                className="max-h-12 md:max-h-20 object-contain"
-              />
-            </a>
-          </div>
 
+        <h2 className="text-center text-3xl md:text-5xl font-seasons_r mt-16 mb-6">
+          TITLE <span className="text-custom-yellow">SPONSOR</span>.
+        </h2>
+        <div className="w-full max-w-screen-xl">
+          <div className="border border-custom-yellow rounded-[15px] p-8 h-28 md:h-56 w-full flex items-center justify-center">
+              <img
+                src={macrosoft}
+                alt="MACROSOFT LOGO"
+                className="max-h-20 md:max-h-40 object-contain mix-blend-screen"
+              />
+          </div>
+        </div>
+
+        <h2 className="text-center text-3xl md:text-5xl font-seasons_r mt-16 mb-6">
+          ASSOCIATE <span className="text-custom-yellow">SPONSORS</span>.
+        </h2>
+        <div className="flex flex-col md:flex-col w-full max-w-screen-xl space-y-4">          
           <div className="border border-custom-yellow rounded-[15px] p-6 h-24 md:h-40 w-full flex items-center justify-center gap-16 md:gap-40">
             <img
               src={cetaaUAE}
@@ -48,6 +45,20 @@ function Sponsors() {
             />
           </div>
 
+          <div className="border border-custom-yellow rounded-[15px] p-8 h-24 md:h-36 w-full flex items-center justify-center">
+            <a
+              href="https://devfolio.co"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={devfolio}
+                alt="DEVFOLIO LOGO"
+                className="max-h-12 md:max-h-20 object-contain"
+              />
+            </a>
+          </div>
+
           <div className="border border-custom-yellow rounded-[15px] p-8 h-24 md:h-32 w-full flex items-center justify-center">
             <img
               src={buildclub}
@@ -58,8 +69,8 @@ function Sponsors() {
         </div>
 
         {/* Community Partners Section */}
-        <h2 className="text-center text-3xl md:text-5xl font-seasons_r mt-12 mb-6">
-          SEASON <span className="text-custom-yellow">PARTNERS</span>.
+        <h2 className="text-center text-3xl md:text-5xl font-seasons_r mt-16 mb-6">
+          SEASON <span className="text-custom-yellow">PARTNER</span>.
         </h2>
         <div className="w-full max-w-screen-xl">
           <div className="border border-custom-yellow rounded-[15px] p-8 h-20 md:h-28 w-full flex items-center justify-center">
